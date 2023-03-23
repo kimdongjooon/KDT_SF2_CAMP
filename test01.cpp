aac9bfb697c43db8cd379c1f579ddc71f83c0e82
kimdongjooon <ehdwns6745@naver.com>
2023-03-23 17:27:29 +0900

    study: initialize c++ file

---
+++ test01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(){
+
+    // std::cout << "Hello world!" ;
+
+    // 자료형 변수 
+    int a = 1;
+    std::cout << "숫자는 " ;
+    std::cout << a ;
+
+    bool b = true;
+    std::cout << '\n';
+    std::cout << b ;
+
+    char c = 'z';
+    std::cout << '\n';
+    std::cout << c ;
+    std::cout << '\n';
+    
+
+    // 문자열 변수 단따옴표는 안됨.
+    std::string str1$ = "안녕하세요" ;
+    std::string str2 = " KDJ입니다." ;
+    str2 = " 김동준입니다.";
+    std::cout << str1$ + str2 ;
+
+
+}
