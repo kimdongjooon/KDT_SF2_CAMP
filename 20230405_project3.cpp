5bc03df77cfe1afd053f353f4efce6ef8fccadc1
kimdongjooon <ehdwns6745@naver.com>
2023-04-06 18:16:20 +0900

    project:20230406

--- 20230405_project3.cpp
+++ 20230405_project3.cpp
@@ -4,17 +4,93 @@
 
 using namespace std;
 
+
 int main(){
     // 랜덤 숫자 맞추기
     // 컴퓨터 랜덤 숫자 생성(1~25) 중복x
     vector <int> com_lst;
     srand(time(NULL));
-    for (int i =0;i<6;i++){
-        int num = rand()%25+1;
-        cout<<num<<" ";
-        com_lst.push_back(num);
+    while(1){
+        int break_fun=0;
+        for (int i =0;i<6;i++){
+            int num = rand()%25+1;
+            com_lst.push_back(num);
+        }
+        //중복 검사
+        for(int i =0; i<com_lst.size();i++){
+           for(int j = 0; j<com_lst.size();j++){
+                if(i!=j && com_lst[i]==com_lst[j]){
+                    break_fun=1;
+                }
+           } 
+        }
+        if(break_fun==1){
+            com_lst.clear();
+        }else{
+            break;
+        }
     }
 
-
+    
     // 사용자 원하는 숫자(1~25) 입력 중복x
+    vector <int> user_lst;
+    int rank_num = 1;
+    while(1){
+        cout<<rank_num<<"번째 숫자를 입력하세요. : ";
+        int num;
+        cin>>num;
+        if(user_lst.empty()){
+            // cout<<"1"<<endl;
+            user_lst.push_back(num);
+            rank_num++;
+        }else{
+            // 중복성 검사, 중복되면 다시 입력 아니면 추가.
+            int dup_count=0;
+            for(int j = 0 ; j<user_lst.size();j++){
+                // cout<<"2 : j = "<<j<<endl;
+                if(user_lst[j]==num){
+                    cout<<j <<" -- "<<user_lst[j] <<" - "<<num; 
+                    cout<<"이미 입력된 숫자입니다. 다시 입력해주세요. "<<endl;
+                    dup_count=1;
+                    break;
+                }
+            }
+            // 중복이 안되면 추가
+            if(dup_count==0){
+                user_lst.push_back(num);
+                rank_num++;
+            }
+
+            // 6개가 다 추가되면 종료
+            if(rank_num==7){
+                break;
+            }
+        }
+        
+    }
+    // 사용자가 입력한 숫자
+    cout<<"사용자 번호 : ";
+    for (int i = 0 ; i < user_lst.size();i++){
+        cout<<user_lst[i]<<" ";
+    }
+    cout<<endl;
+
+    // 맞춘개수 결과!
+    int correct_num=0;
+    for (int i =0; i<com_lst.size();i++){
+        if(com_lst[i]==user_lst[i]){
+            correct_num++;
+        }
+    }
+
+    cout<<"----------------------------"<<endl;
+    cout<<"당첨번호 공개"<<endl;
+    // 컴퓨터가 생성한 숫자
+    for(int i =0; i<com_lst.size();i++){
+        cout<<com_lst[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"결과는 1~7등까지 나옵니다."<<endl;
+    cout<<"결과 : "<<7-correct_num<<"등 입니다 !"<<endl;
+
 }
---
+++ 20230406_project4.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+// #include <algorithm>
+// #include <random>
+#include <cstdlib>
+
+using namespace std;
+ 
+int main(){
+    // 사용자로 부터 홀수 n을 입력 받아 n*n 마방진 만들기
+    // 마방진은 가로 세로 대각선의 합이 같아야 한다.
+
+    vector <int> mabangjin;
+    cout<<"마방진의 행 또는 열 수를 입력해주세요.(홀수만) : ";
+    int num;
+    cin >> num;
+
+    for(int i= 1; i<num*num+1; i++){
+        mabangjin.push_back(i);
+    }
+
+    // 벡터 2차원 초기화
+    vector < vector<int> > td_mabangjin(num,vector<int>(num,0));
+
+    int row=0;
+    int col=num/2;
+    for (int i = 0; i<mabangjin.size();i++){
+        // 첫행 중간열에 삽입.
+        if(i==0){
+            td_mabangjin.at(row).at(col)=mabangjin[i];
+            continue; // i는 1부터 시작하도록 리셋.
+        }
+        // 첫행에 마지막 열일때 아래로 이동
+        if(row==0 && col==num-1){
+            row++;
+            td_mabangjin.at(row).at(col)=mabangjin[i];
+            continue;
+        }
+
+        // 첫행이 0 일때 우상단 이동 맨 아래행의 다음열
+        if(row==0){
+            row=num-1;
+            col++;
+            td_mabangjin.at(row).at(col)=mabangjin[i];
+            continue;
+        }
+
+        // 마지막 열에서 우상단 이동일때 첫번째열에서 이전행
+        if(col==num-1){
+            row--;
+            col=0;
+            td_mabangjin.at(row).at(col)=mabangjin[i];
+            continue;
+        }
+
+        // 만약 우상단으로 이동한 곳이 0이면 값 넣고 0이 아니면 밑으로 이동후 넣기
+        if(td_mabangjin[row-1][col+1]==0){
+            row--;
+            col++;
+            td_mabangjin.at(row).at(col)=mabangjin[i];
+        }else{
+            row++;
+            td_mabangjin.at(row).at(col)=mabangjin[i];
+        }
+
+    }
+
+    for(int i = 0; i<td_mabangjin.size();i++ ){
+        for(int j = 0 ; j<td_mabangjin.at(i).size();j++){
+            cout<<td_mabangjin.at(i).at(j) << " ";
+        }
+        cout<<endl;
+    }
+
+
+}
